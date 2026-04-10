7587239877e5d8021e1bd682160d9f15381bc8c7
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-04-10 23:27:09 +0500

    feat(hero): add hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,6 +15,7 @@ const Hero = () => {
     const transition = { type: 'spring', duration: 3 };
     const mobile = window.innerWidth <= 768 ? true : false;
 
+    
     return (
         <section className="hero" id='hero'>
             <div className="container hero-wrapper">
